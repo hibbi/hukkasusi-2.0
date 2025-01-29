9ee67c8e570bb14b142c5d53a82c5b092e0369b8
hibbi <samuli.halen@gmail.com>
2025-01-29 12:42:07 +0200

    Update hero.js

--- src/pages/paradigmat/parts/hero.js
+++ src/pages/paradigmat/parts/hero.js
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <section className="relative h-screen min-h-[480px] grid pt-[25vh] text-medium font-normal">
       <div className="col-span-4 lg:col-start-7 lg:col-span-6">
-        <h1 className="font-medium flex flex-col">
+        <h1 className="grave text-3xl leading-none md:text-huge flex flex-col">
           <span>Valokuvataiteen koulutus Suomessa:</span>
           <span>Historia ja suuntaviivat 1960-luvulta lähtien</span>
         </h1>
