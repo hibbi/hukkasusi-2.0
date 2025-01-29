9f74feb1a0c2a610b86ac9e12595be9997385c61
hibbi <samuli.halen@gmail.com>
2025-01-29 12:43:48 +0200

    Update page.js

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -45,7 +45,7 @@ export const metadata = {
 export default async function Portfolio() {
       const data = await getPosts();
   return (
-    <div className="grid px-4 pt-12">
+    <div id="portfolio" className="grid px-4 pt-12">
       <div className="flex flex-col col-span-4 md:col-start-3 md:col-end-9">
         {data.edges.map((post) => (
           <Link key={post.node.postId} href={`/portfolio/${post.node.slug}`}>
